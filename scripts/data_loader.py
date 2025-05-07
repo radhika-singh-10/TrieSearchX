4d99cb4a0f943a62b2add72fbeeb7988c5dc34c4
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-07 14:45:52 -0400

    Create data_loader.py

---
+++ scripts/data_loader.py
@@ -0,0 +1,20 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["trie_db"]
+collection = db["words"]
+
+
+collection.delete_many({})
+
+#testing for insertion
+words = [
+    "search", "season", "seal", "see", "seem", "seen", "select",
+    "self", "sell", "semantic", "seminar", "senior", "sense"
+]
+
+
+collection.insert_many([{"word": w} for w in words])
+
+print(f"[INFO] Inserted {len(words)} words into MongoDB.")
