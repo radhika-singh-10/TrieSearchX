7c1cf501980d92b5bf6cd32d2541b1f7ce6cea27
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2025-05-07 14:36:28 -0400

    MongoConnector.cpp

---
+++ src/MongoConnector.cpp
@@ -0,0 +1,19 @@
+#include "../include/MongoConnector.h"
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+#include <bsoncxx/json.hpp>
+#include <iostream>
+
+void MongoConnector::loadWordsToTrie(Trie& trie) {
+    mongocxx::instance inst{};
+    mongocxx::client conn{mongocxx::uri{}};
+    auto collection = conn["trie_db"]["words"];
+
+    for (auto&& doc : collection.find({})) {
+        std::string word = doc["word"].get_utf8().value.to_string();
+        trie.insert(word);
+    }
+
+    std::cout << "[INFO] Loaded words from MongoDB.\n";
+}
